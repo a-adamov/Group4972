94f98ac38ee7aac51993086724e052bb045a57bd
aleada86 <a.adamov86@gmail.com>
2023-09-07 22:01:38 +0300

    added tasks 9, 11, 12, 14, 16

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача №11
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+// Способ 1
+
+void var1()
+{
+    Console.WriteLine("Способ 1");
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(100, 1000);
+
+    Console.WriteLine(number);
+
+    char[] digitChar = number.ToString().ToCharArray();
+    Console.WriteLine(digitChar[0].ToString()+digitChar[2].ToString());
+}
+
+// Способ 2
+
+void var2()
+{
+    Console.WriteLine("Способ 2");
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(100, 1000);
+
+    Console.WriteLine(number);
+
+    Console.Write(number/100);
+    Console.Write(number%10);
+}
+
+var1();
+var2();
---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача №9
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Способ 1
+
+void var1()
+{
+    Console.WriteLine("Способ 1");
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+
+    Console.WriteLine(number);
+
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
+}
+
+// Способ 2
+
+void var2()
+{
+    Console.WriteLine("Способ 2");
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+
+    Console.WriteLine(number);
+
+    Console.WriteLine(number / 10 > number % 10 ? (int)(number / 10) : (int)(number % 10));
+}
+
+// Способ 3
+
+void var3()
+{
+    Console.WriteLine("Способ 3");
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(10, 100);
+
+    Console.WriteLine(number);
+
+    char[] digitChar = number.ToString().ToCharArray();
+    Console.WriteLine(digitChar[0] > digitChar[1] ? digitChar[0] : digitChar[1]);
+}
+
+var1();
+var2();
+var3();
